8c9ff9bbad48884cd88706389b6720d6c5e38f26
arkadiysotnikov <cotnikoarkady@gmail.com>
2022-11-25 16:54:34 +0400

    Add time function to model seasons
    Population increase affected by time, so I added it

--- src/model.py
+++ src/model.py
@@ -1,21 +1,22 @@
-def get_dn_dt(index, B, quantities, increases):
-    if index == 0:
-        return increases[index] * quantities[index] + B[index][index + 1] * quantities[index] * quantities[index + 1]
-    elif index == 1:
-        return increases[index] * quantities[index] + B[index][index - 1] * quantities[index - 1] * quantities[index]
-    return 0
+import math
 
 
-def simulate(B, quantities, increases, time_interval, time_step):
-    first_population_data = [quantities[0]]
-    second_population_data = [quantities[1]]
+def get_dn_dt(index, B, quantities, increases, time_importance, time):
+    summa = 0
+    for i in range(len(quantities)):
+        summa += B[index][i] * quantities[index] * quantities[i]
+    if time_importance != 0:
+        return increases[index] * time_importance * math.sin(time)**2 * quantities[index] + summa
+    return increases[index] * quantities[index] + summa
+
+
+def simulate(B, quantities, increases, time_interval, time_step, time_importance):
+    population_data = [[quantities[i]] for i in range(len(quantities))]
     start = 0
     while start <= time_interval:
         start += time_step
-        dn_dt_0 = get_dn_dt(0, B, quantities, increases)
-        dn_dt_1 = get_dn_dt(1, B, quantities, increases)
-        quantities[0] += dn_dt_0
-        quantities[1] += dn_dt_1
-        first_population_data.append(quantities[0])
-        second_population_data.append(quantities[1])
-    return first_population_data, second_population_data
+        for i in range(len(quantities)):
+            dn_dt = get_dn_dt(i, B, quantities, increases, time_importance, start)
+            quantities[i] += dn_dt
+            population_data[i].append(quantities[i])
+    return population_data
--- src/view.py
+++ src/view.py
@@ -1,6 +1,9 @@
 import numpy as np
 import streamlit as st
 import matplotlib.pyplot as plt
+import pandas as pd
+import plotly.graph_objects as go
+import plotly.express as px
 
 from src.model import simulate
 
@@ -20,7 +23,7 @@ def quantity_of_specie_form(index):
     with col2:
         quantity = st.number_input("Количество", min_value=0, step=1, key=str(index) + "q")
     with col3:
-        increase = st.number_input("Коэффициент прироста", step=0.0001, key=str(index) + "i", format="%.5f")
+        increase = st.number_input("Коэффициент прироста", step=0.01, key=str(index) + "i", format="%.5f")
     return quantity, increase
 
 
@@ -29,7 +32,10 @@ def interaction_coefficients_form(number_of_species):
     grid = make_grid(number_of_species, number_of_species)
     for i in range(number_of_species):
         for j in range(number_of_species):
-            b_matrix[i][j] = grid[i][j].number_input(f"{i}{j}", key=str(i) + str(j) + "interaction", format="%.5f")
+            b_matrix[i][j] = grid[i][j].number_input(f"{i}{j}",
+                                                     key=str(i) + str(j) + "interaction",
+                                                     format="%.5f",
+                                                     step=0.0001)
     return b_matrix
 
 
@@ -38,6 +44,7 @@ def main_page():
     with st.sidebar:
         time_step = st.number_input("Шаг дифференцирования", value=1)
         total_time_interval = st.number_input("Время моделирования", value=1000)
+        time_importance = st.number_input("Коэффициент влияния времени", value=0., step=0.5)
     number_of_species = st.number_input("Введите число видов $$ a_i $$", min_value=1, value=2)
     tab1, tab2 = st.tabs(["Численность популяции и прирост", "Коэффициенты взаимодействия"])
 
@@ -52,9 +59,36 @@ def main_page():
         b_matrix = interaction_coefficients_form(number_of_species)
     print(b_matrix)
 
-    first_data, second_data = simulate(b_matrix, quantities, increases, total_time_interval, time_step)
+    # fig, ax = plt.subplots()
+    x = np.arange(0, total_time_interval + 2, time_step)
+    y = simulate(b_matrix, quantities, increases, total_time_interval, time_step, time_importance=time_importance)
 
-    fig, ax = plt.subplots()
-    ax.plot(np.arange(0, total_time_interval+2, time_step), first_data)
-    ax.plot(np.arange(0, total_time_interval + 2, time_step), second_data)
-    st.pyplot(fig)
+    df = pd.DataFrame()
+    df['time'] = x
+    index = 0
+    for specie in y:
+        df[str(index)] = specie
+        index += 1
+    print(df)
+    df = df.set_index('time')
+
+    fig_without_animation = px.line(df, range_x=[0, total_time_interval])
+    st.plotly_chart(fig_without_animation)
+
+    fig = px.line(df, range_x=[0, total_time_interval])
+    fig.update(frames=[
+        go.Frame(
+            data=[go.Scatter(x=df.index[:k], y=df[str(i)][:k]) for i in range(len(quantities))]
+        )
+        for k in range(0, len(df) + 1, len(df)//100)])
+    # Buttons
+    fig.update_layout(
+        updatemenus=[
+            dict(
+                buttons=list([
+                    dict(label="Play",
+                         method="animate",
+                         args=[None, {"frame": {"duration": 1000}}])
+                ]))])
+
+    st.plotly_chart(fig)
